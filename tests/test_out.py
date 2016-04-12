77824c8a74cb6e6af1871c675786cdedea18793c
Johan Bloemberg <johan.bloemberg@avira.com>
2016-04-12 18:04:27 +0200

    Fix passing params

--- tests/test_out.py
+++ tests/test_out.py
@@ -6,20 +6,20 @@ from helpers import cmd
 def test_out(tmpdir):
     """Test packing image from template."""
 
-    source = {
+    params = {
         'template_path': 'src/ami.json',
     }
 
-    output = cmd('out', source, [str(tmpdir)])
+    output = cmd('out', {}, [str(tmpdir)], params=params)
 
     assert output.get('version').get('ImageId') == 'ami-01234567'
 
 def test_fail(tmpdir):
     """Test packer error is passed."""
 
-    source = {
+    params = {
         'template_path': 'src/non-existing.json',
     }
 
     with pytest.raises(Exception):
-        cmd('out', source, [str(tmpdir)])
+        cmd('out', {}, [str(tmpdir)], params=params)
