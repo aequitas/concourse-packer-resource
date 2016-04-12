9d79f330cd1db194586b3bb3f77e8ec011002601
Johan Bloemberg <johan.bloemberg@avira.com>
2016-04-12 17:54:53 +0200

    Fix failure.

--- tests/test_out.py
+++ tests/test_out.py
@@ -1,3 +1,5 @@
+import pytest
+
 from helpers import cmd
 
 
@@ -11,3 +13,13 @@ def test_out(tmpdir):
     output = cmd('out', source, [str(tmpdir)])
 
     assert output.get('version').get('ImageId') == 'ami-01234567'
+
+def test_fail(tmpdir):
+    """Test packer error is passed."""
+
+    source = {
+        'template_path': 'src/non-existing.json',
+    }
+
+    with pytest.raises(Exception):
+        cmd('out', source, [str(tmpdir)])
